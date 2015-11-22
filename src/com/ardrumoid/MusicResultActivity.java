0438ebfd203060abfc355288a4c01b930cd217b8
seopgyun <team.seopgyun@gmail.com>
2015-11-23 06:42:47 +0900

    Create MusicResultActivity.java

---
+++ src/com/ardrumoid/MusicResultActivity.java
@@ -0,0 +1,16 @@
+package com.ardrumoid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MusicResultActivity extends AppCompatActivity {
+
+	private static final String LOG_TAG = "MusicResultActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_music_result);
+	}
+
+}
