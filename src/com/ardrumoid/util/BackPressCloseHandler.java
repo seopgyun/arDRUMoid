7e71e034e11085289115e4f7bea58705f881ecdb
seopgyun <team.seopgyun@gmail.com>
2015-11-23 06:45:09 +0900

    Create BackPressCloseHandler.java

---
+++ src/com/ardrumoid/util/BackPressCloseHandler.java
@@ -0,0 +1,33 @@
+package com.ardrumoid.util;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+public class BackPressCloseHandler {
+
+    private long backKeyPressedTime = 0;
+    private Toast toast;
+
+    private Activity activity;
+
+    public BackPressCloseHandler(Activity context) {
+        this.activity = context;
+    }
+
+    public void onBackPressed() {
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            showGuide();
+            return;
+        }
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            activity.finish();
+            toast.cancel();
+        }
+    }
+
+    public void showGuide() {
+        toast = Toast.makeText(activity, "한 번 더 누르시면 앱을 종료합니다.", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
